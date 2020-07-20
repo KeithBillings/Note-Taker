d3bdbc49ecac35810cd0145c25222ead40f61724
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-07-20 15:30:27 -0700

    addDeleteNoteFunction

--- server.js
+++ server.js
@@ -20,22 +20,29 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
 // Saving a note
 app.post('/api/notes', function (req, res) {
   let newNote = req.body;
-  let notes = fs.readFileSync(__dirname + '/db/db.json', 'utf-8');
-  notes = JSON.parse(notes);
+  let notes = JSON.parse(fs.readFileSync(__dirname + '/db/db.json', 'utf-8'));
   newNote.id = String(notes.length);
 
   notes.push(newNote);
-  fs.writeFileSync(__dirname+"/db/db.json", JSON.stringify(notes));
+  fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes));
   res.json(notes);
 });
 
+// Deleting a note
+app.delete('/api/notes/:id', function (req, res) {
+  let notes = JSON.parse(fs.readFileSync(__dirname + '/db/db.json', 'utf-8'));
+  const deleteNote = notes.filter(item => item.id != req.params.id);
+
+  fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(deleteNote));
+  res.json(deleteNote);
+});
 
 app.listen(port, () => {
   console.log(`Running on port: ${port}`)
